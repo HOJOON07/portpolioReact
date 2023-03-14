6c6fdda981437440ce1666a2d2e9a1c5116dbab1
hojoon <ghwns107@naver.com>
2023-03-14 13:15:01 +0900

    라우팅 설정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import MusciChart from "./pages/MusciChart";
+import MusicPlayer from "./pages/MusicPlayer";
+import Profile from "./pages/Profile";
+import ProjectList from "./pages/ProjectList";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/MusciChart" element={<MusciChart />} />
+        <Route path="/MusicPlayer" element={<MusicPlayer />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/ProjectList" element={<ProjectList />} />
       </Routes>
     </BrowserRouter>
   );
