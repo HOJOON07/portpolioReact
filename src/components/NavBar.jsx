d24b52b5cb0d77a5891a786958e609e2c1f86ca6
hojoon <ghwns107@naver.com>
2023-03-15 23:32:07 +0900

    cd페이지 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <NavWrap>
+      <LeftBox>
+        <HomeIcon>
+          <HomeBtn>
+            <i />
+          </HomeBtn>
+        </HomeIcon>
+        <SearchWrap>
+          <InputWrap>
+            <SearchInput />
+            <SearchIconWrap>
+              <i />
+            </SearchIconWrap>
+          </InputWrap>
+          {/* result는 컴포넌트 */}
+          <SearchResult />
+        </SearchWrap>
+      </LeftBox>
+      <RightBox>
+        <TerminalIcon>
+          <TerminalBtn>
+            <i />
+          </TerminalBtn>
+        </TerminalIcon>
+        <FullScrennWrap>
+          <FullScreenBtn>
+            <i />
+          </FullScreenBtn>
+        </FullScrennWrap>
+      </RightBox>
+    </NavWrap>
+  );
 }
---
+++ src/pages/cdpage/main.js
@@ -0,0 +1,5 @@
+const musicContents = document.querySelector(".music-contents");
+const musicBtn = document.querySelector(".music-btn");
+const musicCD = document.querySelector(".first-music-cd");
+
+musicBtn.addEventListener("click", function () {});
